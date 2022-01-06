7087df29c6ff07a4175bdfb31558dd8a758ac14d
bertranddibetta <bertrand.dibetta@gmail.com>
2022-01-07 00:02:23 +0100

    Updated game js to run in alert

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+const choices = ["Rock", "Paper", "Scissor"];
+let playerScore = 0;
+let compScore = 0;
+let round = 1;
+
 function computerPlay () {
     i = Math.floor((Math.random() * 3) + 1);{
         if (i === 1) {
@@ -9,8 +14,6 @@ function computerPlay () {
         else return "Scissor";
     }
 }
-
-let playerInput = prompt("Choose between Rock, Paper or Scissor")
 
 // Make playerInput case insensitive
 function fixPlayerInput(input){
@@ -24,29 +27,47 @@ function fixPlayerInput(input){
         return "Scissor";
     }
     else {
-        alert("Your input is incorrect");
+        return "invalid"
     }
 }
 
-// Plays a round of Rock Paper Scissor
-function playRound(playerChoice, computerChoice) {
-    playerIndex = choices.indexOf(playerChoice);
-    computerIndex = choices.indexOf(computerChoice);
-    if (playerSelection === computerChoice){
-        return "This is a tie !" + "\n" + "Computer played " + computerChoice;
-    }
-    else if ((((playerIndex - computerIndex) % 3 + 3 ) % 3) == 1 ){
-        return "You win ! " + playerChoice + " beats " + computerChoice;
-    }
-    else return "You lose ! " + computerChoice + " beats " + playerChoice;
-}
-
+// 
 function game () {
-    playRound(playerSelection, computerSelection)
+    for (round; ; ++round) {
+        if (playerScore == 3 || compScore == 3){
+            break
+        }
+        let playerInput = prompt("Choose between Rock, Paper or Scissor");
+        let playerChoice = fixPlayerInput(playerInput);
+        let compChoice = computerPlay();
+        let playerIndex = choices.indexOf(playerChoice);
+        let compIndex = choices.indexOf(compChoice);
+        if (playerChoice == "invalid"){
+            alert ("Oops, you typed " + playerInput + ". Try again with a correct value !");
+        }
+        else if (playerChoice === compChoice){
+            alert ("This is a tie !" + "\n" + "Computer played " + compChoice + "\n" +
+            "The score is " + playerScore + " for you vs " + compScore + " for the computer.");
+        }
+        else if ((((playerIndex - compIndex) % 3 + 3 ) % 3) == 1 ){
+            ++playerScore;
+            alert ("You win ! " + playerChoice + " beats " + compChoice + "\n" +
+            "The score is " + playerScore + " for you vs " + compScore + " for the computer.");
+        }
+        else { 
+            ++compScore;
+            alert ("You lose ! " + compChoice + " beats " + playerChoice + "\n" +
+            "The score is " + playerScore + " for you vs " + compScore + " for the computer.");
+        }
+    }
+    if (playerScore == 3) {
+        alert ("You win the game !\n" +
+        "The final score is " + playerScore + " for you vs " + compScore + " for the computer.")
+    }
+    else {
+        alert ("You lost the game !\n" +
+        "The final score is " + playerScore + " for you vs " + compScore + " for the computer.")
+    }
 }
 
-const playerSelection = fixPlayerInput(playerInput);
-const computerSelection = computerPlay();
-const choices = ["Rock", "Paper", "Scissor"];
-
-console.log(playRound(playerSelection, computerSelection));
+game();
